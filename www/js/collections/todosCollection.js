2a120ba2d9773fb9ebf275aacc6f97ea1ca9cf56
ioandanpopa <danny18_2006@yahoo.com>
2014-08-07 11:49:06 +0300

    Adds functionallity for adding new items

--- www/js/collections/todosCollection.js
+++ www/js/collections/todosCollection.js
@@ -1,4 +1,7 @@
 App.Collection.TodosCollection = Backbone.Collection.extend({
 	url: './test/spec/fixtures/todo_list.json',
-	model: App.Model.TodoModel
+	model: App.Model.TodoModel,
+	nextId: function() {
+		return this.length ? this.last().get('id') + 1 : 1;
+	}
 });
--- www/js/views/todosCollectionView.js
+++ www/js/views/todosCollectionView.js
@@ -1,6 +1,12 @@
 App.View.TodosCollectionView = Backbone.View.extend({
 	template: _.template($('#todosCollection-template').html()),
-	render: function () {
+	events: {
+		'click #add-button': 'addTodo'
+	},
+	initialize: function() {
+		this.collection.on('all', this.render, this);
+	},
+	render: function() {
 		this.$el.html(this.template());
 		this.collection.forEach( function (todo) {
 				var todoView = new App.View.TodoView({model: todo});
@@ -8,5 +14,12 @@ App.View.TodosCollectionView = Backbone.View.extend({
 				todoView.render();
 		}, this);
 		return this;
+	},
+	addTodo: function (){
+		var title = $('#title-input').val();
+		var nextId = this.collection.nextId();
+		var todoModel = new App.Model.TodoModel({ 'id': nextId, 'title': title, 'completed': false});
+		this.collection.add(todoModel);
 	}
+
 });
